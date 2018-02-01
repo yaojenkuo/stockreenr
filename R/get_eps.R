fefd0c7844f21dae4cc6cf171e5658cdc55b4a4f
yaojenkuo <tonykuoyj@gmail.com>
2018-02-01 10:40:10 +0800

    updated

--- R/get_eps.R
+++ R/get_eps.R
@@ -1,7 +1,8 @@
 #' Get EPS from Statementdog.
 #'
-#' This function will return the EPS of recent 8 quarters of a certain stock.
+#' This function will return the EPS of recent n quarters of a certain stock.
 #' @param x Stock ticker.
+#' @param recent_n Recent n quarters, default 8.
 #' @keywords get_eps
 #' @export
 #' @examples
@@ -12,8 +13,8 @@ get_eps <- function(x, recent_n = 8) {
   this_yr <- Sys.Date() %>%
     format('%Y') %>%
     as.integer()
-  three_yr_ago <- this_yr - 4
-  statement_dog_url <- sprintf("https://statementdog.com/api/v1/fundamentals/%s/%i/1/%i/4/cf?queried_by_user=true", x, three_yr_ago, this_yr)
+  four_yr_ago <- this_yr - 5
+  statement_dog_url <- sprintf("https://statementdog.com/api/v1/fundamentals/%s/%i/1/%i/4/cf?queried_by_user=true", x, four_yr_ago, this_yr)
   statement_data <- fromJSON(statement_dog_url)
   inv_to_ratio <- statement_data$`65`$data[, 2] %>%
     as.numeric()
